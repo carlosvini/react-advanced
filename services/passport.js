4e6f68c0a283f66042488a71bd5c907ccb62c1c7
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-10 18:16:03 -0300

    Lesson 77 - Passport Strategies

--- services/passport.js
+++ services/passport.js
@@ -2,3 +2,19 @@ const passport = require('passport');
 const User = require('../models/user');
 const config = require('../config');
 const { JwtStrategy, ExtractJwt } = require('passport-jwt');
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+    // See if the user ID in the payload exists in our database
+    // If it does, call 'done' with that other
+    // otherwise, call done without a user object
+    User.findById(payload.sub, function(err, user) {
+        if (err) { return done(err, false); }
+
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
+});
