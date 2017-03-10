186a100dd5eee5a446d25b69205721da76406b44
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-10 17:49:40 -0300

    Lesson 72 - Encrypting Passwords with Bcrypt

--- index.js
+++ index.js
@@ -7,13 +7,23 @@ const mongoose = require('mongoose');
 const app = express();
 const router = require('./router');
 
+/*
+const errorHandler = (err, req, res, next) => {
+    res.status(500)
+        .json(err);
+};
+*/
+
 // DB Setup
 mongoose.connect('mongodb://localhost:auth/auth');
 
 // App Setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+
 router(app);
+
+//app.use(errorHandler);
 
 // Server Setup
 const port = process.env.PORT || 3090;
--- models/user.js
+++ models/user.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // define our model
 const userSchema = new Schema({
-    email: { type: String, unique: true, lowercase: true },
-    password: String
+    email: { type: String, unique: true, lowercase: true, required: true },
+    password: { type: String, required: true }
+});
+
+// On Save Hook, encrypt password
+userSchema.pre('save', function (next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) { return next(err); }
+
+        bcrypt.hash(this.password, salt, null, (err, hash) => {
+            if (err) { return next(err); }
+
+            this.password = hash;
+            next();
+        });
+    });
 });
 
 // create the model class
