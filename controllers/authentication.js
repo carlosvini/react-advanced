f93d62c656903c39f0701d0d0ed4c091a70ab4a4
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-10 18:45:09 -0300

    Lesson 84 - Signing Users In

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -13,6 +13,9 @@ function tokenForUser(user) {
 exports.signin = function (req, res, next) {
     // User has already had their email and password auth'd
     // We just need to give them a token
+    res.send({
+        token: tokenForUser(req.user)
+    });
 }
 
 exports.signup = function (req, res, next) {
