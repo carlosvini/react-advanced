07b510c72ed37dd6d293ef3c529685885ca40e61
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-10 18:29:35 -0300

    Lesson 79 - Making an Authenticated Request

--- router.js
+++ router.js
@@ -1,5 +1,12 @@
 const Authentication = require('./controllers/authentication');
+require('./services/passport');
+const passport = require('passport');
 
-module.exports = function (app) {
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+module.exports = function(app) {
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ hi: 'there' });
+    });
     app.post('/signup', Authentication.signup);
 };
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,8 @@
 const passport = require('passport');
 const User = require('../models/user');
 const config = require('../config');
-const { JwtStrategy, ExtractJwt } = require('passport-jwt');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // Setup options for JWT Strategy
 const jwtOptions = {
