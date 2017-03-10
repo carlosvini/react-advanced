270d12512a5ce501e061e6ff4490cc34c154c83c
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-10 15:15:08 -0300

    Lesson 57 - Handling Promises

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,10 +1,18 @@
 export default function ({ dispatch }) {
     return next => action => {
+        // not a promise
         // we dont care about it, send it on
         if (!action.payload || !action.payload.then) {
             return next(action);
         }
 
-        next(action);
+        //  Make sure the action's promise resolves
+        action.payload
+            .then(response => {
+                // create a new action with the same type
+                // replace the promise with the response data
+                const newAction = { ...action, payload: response };
+                dispatch(newAction);
+            });
     };
 }
