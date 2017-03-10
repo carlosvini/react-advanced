989565ff41d9dec98c9daf28052aa650567134f9
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-10 14:19:21 -0300

    Lesson 45 - Handling HOC Edge Cases

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -8,8 +8,19 @@ export default function (ComposedComponent) {
             router: React.PropTypes.object
         };
 
+        componentWillMount() {
+            if (!this.props.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
+        componentWillUpdate(nextProps) {
+            if (!nextProps.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
         render() {
-            console.log(this.props.authenticated);
             return <ComposedComponent {...this.props} />
         }
     }
