708539d08684cf3fe1cc930d5649213679ee4269
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-10 18:40:46 -0300

    Lesson 83 - Protecting Signin Route

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -10,6 +10,11 @@ function tokenForUser(user) {
     }, config.secret);
 }
 
+exports.signin = function (req, res, next) {
+    // User has already had their email and password auth'd
+    // We just need to give them a token
+}
+
 exports.signup = function (req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
