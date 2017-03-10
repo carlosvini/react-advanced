9f13597f282432babd68dfecfcdccb3275fb84b7
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-10 16:28:43 -0300

    Lesson 68 - Inspecting the Database

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
 const router = require('./router');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 // App Setup
 app.use(morgan('combined'));
