687ce37c534e1590719bb389c49e68d2cd37c1be
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-10 14:53:19 -0300

    Lesson 51 - Rendering a List of Users

---
+++ src/components/user_list.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+
+class UserList extends Component {
+    componentWillMount() {
+        this.props.fetchUsers();
+    }
+
+    renderUser(user) {
+        return (
+            <div className="car card-block" key={user.name}>
+                <h4 className="car-title">{user.name}</h4>
+                <p className="card-text">Cheese Factory</p>
+                <a className="btn btn-primary">Email</a>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.users.map(this.renderUser)}
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return { users: state.users };
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UserList);
