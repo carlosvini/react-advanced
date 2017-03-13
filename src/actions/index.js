a7cfee28e39ccdbc05fb7f2cdf215a1b2a66796d
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-13 14:30:21 -0300

    Lesson 101 - Auth Error Messaging

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER } from './types';
+import {
+    AUTH_USER,
+    AUTH_ERROR
+} from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -20,6 +23,14 @@ export function signinUser({ email, password }) {
             .catch(() => {
                 // If request is bad...
                 // - Show an error to the user
+                dispatch(authError('Bad Login Info'));
             });
     }
+}
+
+export function authError(error) {
+    return {
+        type: AUTH_ERROR,
+        payload: error
+    };
 }
