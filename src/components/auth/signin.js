4f5bd885732fedd367cd80ea5db611cc4e734675
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-13 14:33:13 -0300

    Lesson 102 - Displaying Errors

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -9,6 +9,16 @@ class Signin extends Component {
         this.props.signinUser({ email, password })
     }
 
+    renderAlert() {
+        if (this.props.errorMessage) {
+            return (
+                <div className="alert alert-danger">
+                    <strong>Oops!</strong> {this.props.errorMessage}
+                </div>
+            );
+        }
+    }
+
     render() {
         const { handleSubmit, fields: { email, password }} = this.props;
 
@@ -22,14 +32,18 @@ class Signin extends Component {
                     <label>Password:</label>
                     <input {...password} type="password" className="form-control" />
                 </fieldset>
+                {this.renderAlert()}
                 <button action="submit" className="btn btn-primary">Sign in</button>
             </form>
         );
     }
 }
 
+function mapStateToProps(state) {
+    return { errorMessage: state.auth.error };
+}
 
 export default reduxForm({
     form: 'signin',
     fields: ['email', 'password']
-}, null, actions)(Signin);
+}, mapStateToProps, actions)(Signin);
