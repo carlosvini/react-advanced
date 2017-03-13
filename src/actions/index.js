68b45d601bf25f4d93beaaa094885eda0b033951
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-13 10:29:48 -0300

    Lesson 92 - Action Creator WIth Many Responsibilities

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+
+export function signinUser({ email, password }) {
+    // Submit email/password to the server
+
+    // If request is good...
+    // - Update state to indicate user is authenticated
+    // - Save the JWT token
+    // - redirect to the route '/feature'
+
+    // If request is bad...
+    // - Show an error to the user
+}
