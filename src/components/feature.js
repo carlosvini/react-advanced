cb80d7ab4e5570f185729c4113eafb995711e6bf
Carlos Rodrigues <carlos@jp7.com.br>
2017-03-13 15:16:08 -0300

    Lesson 116 - Making Authenticated API requests

--- src/components/feature.js
+++ src/components/feature.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Feature extends Component {
+    componentWillMount() {
+        this.props.fetchMessage();
+    }
+
     render() {
         return (
-            <div>This is a feature</div>
+            <div>{this.props.message}</div>
         );
     }
 }
 
-export default Feature;
+export default connect(null, actions)(Feature);
